cc30aaf26b7d5bde22a7901809f67dc99e97c895
GabiCanteros <gabriel.canteros@widergy.com>
2025-03-21 15:32:40 -0300

    fix: migrations-update

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,17 +6,16 @@
 #  title      :string
 #  content    :string
 #  note_type  :string
+#  user_id    :bigint(8)        not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :bigint(8)        not null
 #
 class Note < ApplicationRecord
   belongs_to :user
 
-
   validates :title, presence: true
   validates :content, presence: true
-  validates :note_type, presence: true, inclusion: { in: ['review', 'critique'] }
+  validates :note_type, presence: true, inclusion: { in: %w[review critique] }
 
   has_one :utility, through: :user
 end
