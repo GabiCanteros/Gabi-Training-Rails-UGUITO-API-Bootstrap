6e6b5325107ebbe6f24b7f57c45840f8a4f53f50
GabiCanteros <gabriel.canteros@widergy.com>
2025-03-26 11:29:01 -0300

    feat:[TR-800] create note model (#1)
    * feat: add-note-model
    
    * feat: blank commit
    
    * fix: migrations-update
    
    * fix: added destroy behavior, line simplification and space removal

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -11,7 +11,6 @@ class HealthCheckController < HealthCheck::HealthCheckController
 
     @utility_response = utility.utility_service.health_check
     return super if @utility_response.code == 200
-    
 
     render_failed_utility_health_check
   end
