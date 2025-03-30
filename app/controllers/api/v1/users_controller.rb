438f89b925afdd463d50e6ff15043ffa7c555a37
GabiCanteros <gabriel.canteros@widergy.com>
2025-03-30 09:21:31 -0300

    feat: complete information for GET of User (serializer updated)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,10 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      before_action :authenticate_user!
+      before_action :authenticate_user! #comentar esta linea
 
       def current
-        render json: current_user, status: :ok, serializer: UserSerializer
+        render json: current_user, status: :ok, serializer: UserSerializer #reemplazar User.find(1) por current_user
       end
     end
   end
