2da976d01260937327cdcd2b6452ed63f48e989b
GabiCanteros <gabriel.canteros@widergy.com>
2025-04-04 14:37:37 -0300

    feat:[TR-803] Index and Show services for Note (#4)
    * feat: serializers create to list and find notes - notes controller created to services of show and index
    
    * fix: Removed spaces and code ordered
    
    * fix: serializers update - routes manage update - validations add
    
    * fix: corrections in methods design
    
    * fix: changes at validations and error manage - a serializer alias update

---
+++ app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,66 @@
+module Api
+  module V1
+    class NotesController < ApplicationController
+      before_action :validate_order_param, :validate_page_params, only: [:index]
+
+      def index
+        render json: notes_filtered, status: :ok, each_serializer: IndexNoteSerializer
+      end
+
+      def show
+        render json: show_note, status: :ok
+      end
+
+      private
+
+      def validate_order_param
+        return unless order.present? && !%w[asc desc].include?(order)
+        render json: { error: I18n.t('errors.invalid_order_param') },
+               status: :unprocessable_entity
+      end
+
+      def validate_page_params
+        return render_page_error unless params[:page].present? && valid_number?(params[:page])
+
+        render_page_error unless params[:page_size].present? && valid_number?(params[:page_size])
+      end
+
+      def render_page_error
+        render json: { error: I18n.t('errors.invalid_page_param') },
+               status: :unprocessable_entity
+      end
+
+      def valid_number?(value)
+        value.to_i.positive?
+      end
+
+      def notes_filtered
+        notes.where(filtering_params)
+             .order(created_at: order)
+             .page(params[:page])
+             .per(params[:page_size])
+      end
+
+      def notes
+        Note.all
+      end
+
+      def filtering_params
+        params_transformed.permit(%i[note_type title])
+      end
+
+      def params_transformed
+        param_mapping = { 'type' => 'note_type' }
+        params.transform_keys! { |key| param_mapping[key] || key }
+      end
+
+      def order
+        @order ||= params[:order] || 'asc'
+      end
+
+      def show_note
+        notes.find(params.require(:id))
+      end
+    end
+  end
+end
---
+++ app/serializers/note_serializer.rb
@@ -0,0 +1,5 @@
+class NoteSerializer < ActiveModel::Serializer
+  attributes :id, :title, :content, :word_count, :content_length, :created_at
+  attribute :note_type, key: :type
+  belongs_to :user, serializer: UserSerializer
+end
