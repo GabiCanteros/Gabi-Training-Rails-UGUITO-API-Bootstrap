a4dd2716fed9d232f12620d2ea8bbf3d96ef9ad6
GabiCanteros <gabriel.canteros@widergy.com>
2025-04-04 15:39:33 -0300

    fix: comments removed

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,10 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      before_action :authenticate_user! #comentar esta linea
+      before_action :authenticate_user!
 
       def current
-        render json: current_user, status: :ok, serializer: UserSerializer #reemplazar User.find(1) por current_user
+        render json: current_user, status: :ok, serializer: UserSerializer 
       end
     end
   end
